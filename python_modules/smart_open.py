2c1bb81cbeed731957c5638941e00b8ace3f3bd8
allydunham <ally@ebi.ac.uk>
2019-01-09 13:55:48 +0000

    Added smart_open function to handle opening files that may be stdout or already a file

---
+++ python_modules/smart_open.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Function for versatile opening of file/file paths/stdout to write to
+"""
+import sys
+import io
+from contextlib import contextmanager
+
+@contextmanager
+def open_file(path, mode='w'):
+    """Context manager managing a specified file (via a path or file_object) or sys.stdout"""
+    close = False
+    if not path:
+        file_handle = sys.stdout
+    elif isinstance(path, io.IOBase):
+        file_handle = path
+    else:
+        file_handle = open(path, mode)
+        close = True
+    yield file_handle
+    if close:
+        file_handle.close()
+
+if __name__ == '__main__':
+    pass
