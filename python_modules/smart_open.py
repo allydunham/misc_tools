30de8ca0ffb4b0cb69a11fda0db95e5f1bd0bd68
allydunham <ally@ebi.ac.uk>
2019-01-09 14:57:15 +0000

    Added *args to smart_open to allow passing unnamed arguments to open

--- python_modules/smart_open.py
+++ python_modules/smart_open.py
@@ -7,15 +7,16 @@ import io
 from contextlib import contextmanager
 
 @contextmanager
-def smart_open(path, **kwargs):
-    """Context manager managing a specified file (via a path or file_object) or sys.stdout"""
+def smart_open(path, *args, **kwargs):
+    """Context manager managing a specified file (via a path or file_object) or sys.stdout.
+       Additional arguments are passed through to open()"""
     close = False
     if not path:
         file_handle = sys.stdout
     elif isinstance(path, io.IOBase):
         file_handle = path
     else:
-        file_handle = open(path, **kwargs)
+        file_handle = open(path, *args, **kwargs)
         close = True
     yield file_handle
     if close:
